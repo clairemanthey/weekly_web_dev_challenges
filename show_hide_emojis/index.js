f57eebe67ad2789c29e0c6d0f08e6d45a62b0d01
Claire <63174119+clairemanthey@users.noreply.github.com>
2021-07-14 18:42:23 -0500

    Added files
    Add htm, css, and js files frow show/hide emojis

---
+++ show_hide_emojis/index.js
@@ -0,0 +1,12 @@
+// code goes here
+  
+let show = document.getElementById("emojis");
+
+function toggleVisibility() {
+  if(show.style.display == "none"){
+	  show.style.display = "flex";
+  } else{
+	  show.style.display = "none";
+  }
+ //and here
+}
