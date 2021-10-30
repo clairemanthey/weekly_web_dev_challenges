4fe581fb32ae6c44bd7905586b37d6d74ebcdf89
Claire <63174119+clairemanthey@users.noreply.github.com>
2021-10-30 12:50:34 -0500

    Uploaded files
    Uploaded html, css, and js files for mood_for_food

---
+++ mood_for_food/index.js
@@ -0,0 +1,16 @@
+// Remember: You rock! 🎸
+//api https://foodish-api.herokuapp.com/images/dessert
+
+
+document.getElementById("btn").addEventListener("click", function() {
+    fetch('https://foodish-api.herokuapp.com/api/images/dessert')
+    .then(response => response.json())
+    .then(photo => {
+        document.getElementById("dessert").src = photo.image;
+    });
+});
+
+document.getElementById("btn").addEventListener("click", function(){
+    document.getElementById("dessertTxt").innerHTML = "Does it look good?";
+});
+
