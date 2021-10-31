45afece1301e2528477fcd362c444c2005866a88
Claire <63174119+clairemanthey@users.noreply.github.com>
2021-10-31 12:53:45 -0500

    Uploaded files
    Uploaded html, css, and js index files to dropDownNave

---
+++ dropDownNave/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("overlay").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("overlay").style.height = "0%";
+}
