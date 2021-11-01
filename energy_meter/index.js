793da429e1e839439acd0048c01c643783643093
Claire <63174119+clairemanthey@users.noreply.github.com>
2021-10-31 23:51:47 -0500

    Uploaded files
    Uploaded js index file for enegry_meter

---
+++ energy_meter/index.js
@@ -0,0 +1,23 @@
+let meter = document.getElementById("energy");
+let energyAmount = document.getElementById("energyInput");
+
+
+energyAmount.addEventListener("input", function(){
+	meter.value = energyAmount.value;
+});
+
+
+let exerciseAmount = document.getElementById("exerciseTime");
+let exerciseTime = document.getElementById("exercise");
+
+exerciseTime.addEventListener("input", function(){
+	exerciseAmount.value = exerciseTime.value;
+});
+
+
+let sleepAmount = document.getElementById("sleep");
+let sleep = document.getElementById("sleepTime");
+
+sleepAmount.addEventListener("input", function(){
+	sleep.value = sleepAmount.value;
+});
